9d1925bd38c98d738d52b90f5b7f78fd51033d7d
Kas <ksixbones@gmail.com>
2021-02-10 21:50:13 -0700

    alcoholimage-stuff

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Project-One
+# What are you drinking App  (Project - One)
+
+We are making a web application that allows you to enter a favorite type of alcohol and then it brings back drink and food selections incorporating that drink.
+
+User story - As a drink enthusiast that desires a good drink/food recipe that incorporates a specific type of alcohol... I want to select a type of alcohol to be included in a food or drink recipe. I would like an app that automatically pulls up a list of food and drink recipes for me when I input a type of alcohol.
+
+App Details:
+
+Description - Application that pulls a list of food recipes with user inputed alcohol as an ingredient. Runs same operation with drink recipes. You get to select if you want drinks or food to appear in your search(one or the other).
+Wireframe(s) - https://drive.google.com/file/d/1e2toM242yOffjQnCIl6N694XNcNHUKJw/view?usp=sharing
+
+APIs identified and at least one Postman request in place - Spoonacular and Cocktail DB API's
+
+
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,91 +1,159 @@
 // Global variables to set...
+
+var alcohol = $('#alcohol-name').val()
+var alcoholThumbnail = $('.thumbnail')
+
 // var: multiple of these to link with html elements that will hold the API data and display on website
 
 // functions to use...
 // .on("click") for alcohol selection
 // .setItem() for alcohol selection into local storage
-$("#alcohol-search").on("click", function getAlcoholData (event) {
 
-    // clearCurrent();
-    event.preventDefault();
+// function() for search history displayal on screen
+// if (localStorage.getItem("alcohol")) {
+//     createSearchHistory(localStorage.getItem("alcohol"));
+// };
 
-    var alcoholName = $("#alcohol-name").val();
 
-    localStorage.setItem("alcohol", alcoholName);
-    createSearchHistory(alcoholName);
 
-    searchRecipePup(alcoholName);
-    searchSpoon(alcoholName);
-});
+$('#alcohol-search').on('click', function getAlcoholData (event) {
+  // clearCurrent();
+  event.preventDefault()
 
+  var alcoholName = $('#alcohol-name').val()
+
+  localStorage.setItem('alcohol', alcohol)
+
+  getAlcoholImage(alcoholName)
+  //searchRecipePup(alcoholName);
+  //searchSpoon(alcoholName);
+})
 
 // .fetch() for drink recipes
 // Parameters will be...
 // user selected alcohol
-function searchRecipePup(alcohol) {
+// function searchRecipePup(alcohol) {
 
-    fetch('http://www.recipepuppy.com/api/?i=' + alcohol + '&q=meal', {
-        credentials: 'same-origin',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-        },
-    })
+//     fetch('http://www.recipepuppy.com/api/?i=' + alcohol + '&q=meal', {
+//         credentials: 'same-origin',
+//         headers: {
+//             'Accept': 'application/json',
+//             'Content-Type': 'application/json',
+//         },
+//     })
 
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-        })
-};
-
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             console.log(data);
+//         })
+//     }
 
 // .fetch() for food recipes
 // Parameters will be...
 // User selected alcohol
-function searchSpoon(alcohol) {
+// function searchSpoon(alcohol) {
 
-    fetch('https://api.spoonacular.com/recipes/complexSearch?type=drink&addRecipeInformation=true&includeIngredients=' + alcohol + '&apiKey=ead0fc7909d64d048c3a9e73cccc830f')
+//     fetch('https://api.spoonacular.com/recipes/complexSearch?type=drink&addRecipeInformation=true&includeIngredients=' + alcohol + '&apiKey=ead0fc7909d64d048c3a9e73cccc830f')
 
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (foodData) {
-            console.log(foodData);
-        })
-};
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (foodData) {
+//             console.log(foodData);
+//         })
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // function() to display photo of selected alcohol on screen
-    // function displayImage() {
+function getAlcoholImage (alcohol) {
+    var myHeaders = new Headers();
+    myHeaders.append("Cookie", "__cfduid=d6ca9d40b18797da336304f25e85fd21d1612407137");
+    
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow'
+    };
+    
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" +alcohol, requestOptions)
+      .then(response => response.text())
+      .then(result => {
+          var data = JSON.parse(result);
+        console.log(data.drinks[0].strDrinkThumb);
+        var alcoholUrl = data.drinks[0].strDrinkThumb
+        $("#alcohol-pic").attr("src", alcoholUrl)
+      })
+      .catch(error => console.log('error', error));
 
-    // };
+      
+}
 
 
-// function() for search history displayal on screen
-if (localStorage.getItem("alcohol")) {
-    createSearchHistory(localStorage.getItem("alcohol"));
-};
 
-function createSearchHistory(city) {
 
-    var historyBtn = $("<li>", {"class": "button"}).text(city);
-    $("#history-search-btn").prepend(historyBtn);
-};
+
+//   // fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + alcohol)
+//   fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + alcohol + '')
+
+//           .then(function (response) {
+//               console.log(response);
+//             return response.json();
+            
+//         })
+//         .then(function () {
+//             console.log(data);
+//         })
+// };
+
+
+//   .then(result => result.text())
+//   .then(data => console.log(data))
+// }
+
+
+//     var alcoholImage = getAlcoholImage (alcohol).data
+//     console.log(alcoholImage[1].strDrinkThumb)
+  
+//   $('alcoholThumbnail').append(alcoholImage);
+  
+
+ // $("#history-search-btn").click(function() {
+    // ":first" and ":first-child" will work.
+  //      $(".alcoholThumbnail").append("drinks.child");
+  
+
+
+
+//
+
+getAlcoholImage(alcohol)
+//displaylImage(alcoholName);
 
 // runs API's and displays results on screen when history button is clicked
-$("#history-search-btn").click(function (event) {
+$('#history-search-btn').click(function (event) {
+  getAlcoholImage(alcohol)
 
-    // clearCurrent();
-    localStorage.setItem("alcohol", event.target.textContent);
+  // clearCurrent();
+  //localStorage.setItem("alcohol", event.target.textContent);
 
-    searchRecipePup(event.target.textContent);
-    searchSpoon(event.target.textContent);
-});
-
+  // searchRecipePup(event.target.textContent);
+  // searchSpoon(event.target.textContent);
+})
 
 // function() to clear previous displayed API info so that the new search API info can take its place on the website
-    // function clearCurrent() {
-
-    // };
+// function clearCurrent() {
